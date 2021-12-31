b9f3723ab93818f90845144e361d63102545ed27
Akaniru Eunice <chieunice114@gmail.com>
2021-12-31 12:16:59 -0800

    created a middleware for the token authenticaltion

--- controllers/main.js
+++ controllers/main.js
@@ -18,11 +18,9 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-	const luckyNumber = Math.floor(Math.random() * 100);
-	res
-		.status(200)
-		.json({
-			msg: `Hello, Eunice`,
+    const luckyNumber = Math.floor(Math.random() * 100);
+		res.status(200).json({
+			msg: `Hello, ${req.user.username}`,
 			secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
 		});
 };
---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
+const authenticationMiddleware = async (req, res, next) => {
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith("Bearer ")) {
+		throw new CustomAPIError("no token provided", 401);
+	}
+	const token = authHeader.split(" ")[1];
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		const {id, username} = decoded
+        req.user = {id, username}
+	} catch (error) {
+		throw new CustomAPIError("not authorized to access this route", 401);
+	}
+	next();
+};
+
+module.exports = authenticationMiddleware;
