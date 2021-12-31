fc62b16a9bf58f64aaeda183e472283abc62182c
Akaniru Eunice <chieunice114@gmail.com>
2021-12-31 11:35:12 -0800

    installed the jsonwebtoken,created a .env file and initialized the jwt and get back a token

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,33 @@
+
+const jwt = require("jsonwebtoken");
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
-    res.send('fake Login/REgister/signup route')
-}
+	const { username, password } = req.body;
+	if (!username || !password) {
+		throw new CustomAPIError("Please provide email and password", 400);
+	}
+    //just for demo, normally provideg by DB!!!
+    const id = new Date().getDate()
+
+    //try to keep payload small, better exprices for user
+    // just for demo, in production use long, complex and unguessable string value !!!!
+    const token = jwt.sign({id, username},process.env.JWT_SECRET,{expiresIn:"30d"})
+
+	res.status(200).json({msg:"user created", token});
+};
 
 const dashboard = async (req, res) => {
-    const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg:`Hello, Eunice`, secret:`Here is your authorized data, your lucky number is ${luckyNumber}`})
-}
+	const luckyNumber = Math.floor(Math.random() * 100);
+	res
+		.status(200)
+		.json({
+			msg: `Hello, Eunice`,
+			secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+		});
+};
 
 module.exports = {
-    login,
-    dashboard
-}
+	login,
+	dashboard,
+};
